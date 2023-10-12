b7bef92e10174784a532789a1a613d891482b80b
Yusuf Rizal <yusufrizalh@gmail.com>
2023-10-12 15:38:12 +0700

    flask recipe

--- models/recipe.py
+++ models/recipe.py
@@ -29,3 +29,30 @@ class Recipe(db.Model):
         onupdate=db.func.now(),
     )
     user_id = db.Column(db.Integer(), db.ForeignKey("user.id"))
+
+    def data(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "description": self.description,
+            "num_of_servings": self.num_of_servings,
+            "cook_time": self.cook_time,
+            "directions": self.directions,
+            "user_id": self.user_id,
+        }
+
+    @classmethod
+    def get_all_published(cls):
+        return cls.query.filter_by(is_publish=True).all()
+
+    @classmethod
+    def get_by_id(cls, recipe_id):
+        return cls.query.filter_by(id=recipe_id).first()
+
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
